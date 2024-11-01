cef99a2377e060dd46e711c7e06d4889d8db0cdb
Mosharof <md5470415@gmail.com>
2024-11-01 11:09:14 +0600

    javascript uinit-1 push

---
+++ JavaScript/unit-1/programming-Basic.js
@@ -0,0 +1,22 @@
+// 1. Printing an output with console.log()
+// 2. Declaring variables with let, var, and const keywords
+// 3. Statements and syntax
+// 4. Comments
+// 5. Operators
+/* **
+    i. Assignment operators,
+    ii.  Arithmetic operators,
+    iii. Comparative operators,
+    iv. Logical operators.
+    V. type Operators (typeof, instanceof)
+** */
+
+// 6. Data types:
+/*
+I. Primitive data types,
+    // Primitive: a) number, b) string c) boolean d) null,
+    e) undefined … 
+
+ii. Object types
+    // Object: a) Array, b) Object, c) Function, d) map e), Set, f) Weak Set, g) Weak Map 
+*/
